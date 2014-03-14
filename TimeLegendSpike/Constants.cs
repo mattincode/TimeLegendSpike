8817d1f10dd8cb9d203c8eb850fb459ba03e2210
titte <titte@telia.com>
2014-03-14 10:01:31 +0100

    Working timescroll

---
+++ TimeLegendSpike/Constants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace TimeLegendSpike
+{
+    public static class Constants
+    {
+        public const int TIncMinutes = 30; // 30 minutes
+        public const int VIncPx = 15;
+        public const int VTextOffset = 0;
+
+    }
+}
---
+++ TimeLegendSpike/ViewModels/MainViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics;
+using Jounce.Core.Command;
+using Jounce.Framework.Command;
+
+namespace TimeLegendSpike.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private DateTime _start;
+        private DateTime _end;
+
+        public DateTime Start
+        {
+            get { return _start; }
+            set { _start = value; RaisePropertyChanged(() => Start); }
+        }
+
+        public DateTime End
+        {
+            get { return _end; }
+            set { _end = value; RaisePropertyChanged(() => End); }
+        }
+
+        public DateTime PeriodStart { get; set; }
+        public DateTime PeriodEnd { get; set; }
+        public IActionCommand NavigateBackCommand { get; private set; }
+        public IActionCommand NavigateForwardCommand { get; private set; }
+
+        public MainViewModel()
+        {
+            Start = new DateTime(2014,03,1,10,00,00);
+            PeriodStart = Start.AddMinutes(-60);
+            PeriodEnd = Start.AddDays(2);
+            NavigateBackCommand = new ActionCommand<object>(OnNavigateBack, CanExecuteNavigateBack);
+            NavigateForwardCommand = new ActionCommand<object>(OnNavigateForward, CanExecuteNavigateForward);
+        }
+
+        private bool CanExecuteNavigateBack(object o)
+        {
+            var canExecute = (Start > PeriodStart);
+            Debug.WriteLine("CanExecuteNavigateBack: {0}", canExecute);
+            return canExecute;
+        }
+
+        private void OnNavigateBack(object o)
+        {
+            if (Start == PeriodStart)
+                return;
+            var newStart = Start.AddMinutes(-Constants.TIncMinutes);
+            Start = newStart < PeriodStart ? PeriodStart : newStart;
+        }
+
+        private bool CanExecuteNavigateForward(object o)
+        {
+            bool canExecute = (End < PeriodEnd);
+            Debug.WriteLine("CanExecuteNavigateForward: {0}", canExecute);
+            return canExecute;
+        }
+
+        private void OnNavigateForward(object o)
+        {
+            if (End == PeriodEnd)
+                return;
+            var newEnd = End.AddMinutes(Constants.TIncMinutes);
+            Start = newEnd > PeriodEnd ? PeriodStart : Start.AddMinutes(Constants.TIncMinutes);
+        }
+    }
+
+}
