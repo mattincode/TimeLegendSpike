a197513230e1bfb66863d3d9e9f19074d055f759
titte <titte@telia.com>
2014-04-17 14:50:40 +0200

    Added groupcontrol

---
+++ TimeLegendSpike/AirportStaffingPlanningGroupControl.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using TimeLegendSpike.ViewModels;
+
+namespace TimeLegendSpike
+{
+    public partial class AirportStaffingPlanningGroupControl : UserControl
+    {
+        public AirportStaffingPlanningGroupControl()
+        {
+            InitializeComponent();
+        }
+
+        #region Dependency properties
+        public static readonly DependencyProperty PeriodStartProperty = DependencyProperty.Register("PeriodStart", typeof(DateTime), typeof(UserControl), new PropertyMetadata(new PropertyChangedCallback(PeriodStartChanged)));
+        public DateTime PeriodStart
+        {
+            get { return (DateTime)GetValue(PeriodStartProperty); }
+            set
+            {
+                SetValue(PeriodStartProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty PeriodEndProperty = DependencyProperty.Register("PeriodEnd", typeof(DateTime), typeof(UserControl), new PropertyMetadata(new PropertyChangedCallback(PeriodEndChanged)));
+        public DateTime PeriodEnd
+        {
+            get { return (DateTime)GetValue(PeriodEndProperty); }
+            set
+            {
+                SetValue(PeriodEndProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty BookingsProperty = DependencyProperty.Register("Bookings", typeof(ObservableCollection<Booking>), typeof(UserControl), new PropertyMetadata(null));
+        public ObservableCollection<Booking> Bookings
+        {
+            get { return (ObservableCollection<Booking>)GetValue(BookingsProperty); }
+            set
+            {
+                SetValue(BookingsProperty, value);
+            }
+        }
+
+        private static void PeriodStartChanged(DependencyObject sender, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var ctl = sender as AirportStaffingPlanningGroupControl;
+            var newStart = dependencyPropertyChangedEventArgs.NewValue as DateTime?;
+            ctl.UpdatePeriodChanged(newStart, null);
+        }
+
+        private static void PeriodEndChanged(DependencyObject sender, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var ctl = sender as AirportStaffingPlanningGroupControl;
+            var newEnd = dependencyPropertyChangedEventArgs.NewValue as DateTime?;
+            ctl.UpdatePeriodChanged(null, newEnd);
+        }
+        #endregion
+
+        
+        private void AirportStaffingPlanningGroupControl_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            BookingCanvas.Children.Clear();
+            foreach (var booking in Bookings)
+            {
+                BookingCanvas.Children.Add(new AirportStaffingBookingControl()
+                {
+                    Booking = booking,
+                    PeriodStart = PeriodStart,
+                    PeriodEnd = PeriodEnd
+                });
+
+            }       
+        }
+
+        private void UpdatePeriodChanged(DateTime? periodStart, DateTime? periodEnd)
+        {
+            if (periodStart.HasValue)
+            {
+                foreach (var control in BookingCanvas.Children)
+                {
+                    var bookingControl = control as AirportStaffingBookingControl;
+                    if (bookingControl.PeriodStart != periodStart.Value)
+                        bookingControl.PeriodStart = periodStart.Value;
+                }
+            }
+            else if (periodEnd.HasValue)
+            {
+                foreach (var control in BookingCanvas.Children)
+                {
+                    var bookingControl = control as AirportStaffingBookingControl;
+                    if (bookingControl.PeriodEnd != periodEnd.Value)
+                        bookingControl.PeriodEnd = periodEnd.Value;
+                }
+            }
+        }
+    }
+}
